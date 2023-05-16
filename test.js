e2889dbb8e3eb3b58045d7126d98476fbacedacc
jangrubin <wkdfnqls2465@gmail.com>
2023-05-16 11:19:02 +0900

    test.ts compile

---
+++ test.js
@@ -0,0 +1,6 @@
+var a = 1;
+var b = 2;
+function test(a, b) {
+    return a + b;
+}
+console.log(test(a, b));
---
+++ test.ts
@@ -0,0 +1,7 @@
+var a = 1;
+var b = 2;
+
+function test(a,b) {
+  return a + b;
+}
+console.log(test(a,b));
