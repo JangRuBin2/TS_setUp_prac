f8e9be6cf8ab2ec02208126a119ed8de060a5c93
jangrubin <wkdfnqls2465@gmail.com>
2023-05-16 11:55:33 +0900

    뜬금 없이 데이터란 변수를 export??

--- test.ts
+++ test.ts
@@ -1,7 +1,13 @@
-var a = "1";
+var a = 1;
 var b = 2;
+
+// vodi -> 회피하다? return이 없다.
+function test2() : void {
+  console.log("hi")
+}
 
 function test(a: number,b: number) : number {
   return a + b;
 }
 console.log(test(a,b));
+test2()
