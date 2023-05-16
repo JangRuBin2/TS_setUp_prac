17a82497a1c5acee12ad17da9f8a24965e91c393
jangrubin <wkdfnqls2465@gmail.com>
2023-05-16 11:50:59 +0900

    check typeError sign

--- test.js
+++ test.js
@@ -1,4 +1,4 @@
-var a = 1;
+var a = "1";
 var b = 2;
 function test(a, b) {
     return a + b;
--- test.ts
+++ test.ts
@@ -1,7 +1,7 @@
-var a = 1;
+var a = "1";
 var b = 2;
 
 function test(a: number,b: number) : number {
   return a + b;
 }
-console.log(test(1,2));
+console.log(test(a,b));
